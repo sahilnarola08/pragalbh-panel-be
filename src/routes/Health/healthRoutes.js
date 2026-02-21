881225e846f07cf55d9b5406bca86e0245e2cf7e
Yash Dhameliya <yashdhameliya98@gmail.com>
2026-02-21 14:51:43 +0530

    Update health route to use root path for health check endpoint

--- src/routes/Health/healthRoutes.js
+++ src/routes/Health/healthRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import healthController from "../../controllers/healthController.js";
 
-router.get("/test", healthController.createHealth);
+router.get("/", healthController.createHealth);
 
 
 export default router;
